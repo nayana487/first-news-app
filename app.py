5a0076e82b0a3c736cadd25e2c01a9cb0744248d
Nayana Davis <nayana487@gmail.com>
2014-04-24 18:59:59 -0400

    Created basic table

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ app = Flask(__name__)
 csv_path = './static/la-riots-deaths.csv'
 csv_obj = csv.DictReader(open(csv_path, 'r'))
 csv_list = list(csv_obj)
+http://localhost:8000/1/
 
 @app.route("/")
 def index():
     return render_template('index.html',
         object_list=csv_list,
     )
-
+@app.route('/<number>/')
+def detail(number):
+      return render_template('detail.html',
+        object=csv_dict[number],
+    )
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
